86da23b87f031709b8cfecc0142bfea9578bd273
A H Sheikh <abdulhaadi0307@outlook.com>
2025-10-13 13:32:41 +0200

    MongoDB test (TLS/certifi) OK; Spark⇄Cassandra verifisert; pipeline Cassandra→Mongo lagt til; oppdatert .gitignore for secrets

--- Ass_2/mongo_test.py
+++ Ass_2/mongo_test.py
@@ -1,14 +1,10 @@
+import os, certifi
 from pymongo import MongoClient
-from datetime import datetime, UTC
 
-MONGO_URI = "mongodb+srv://AHS_db_user:<db_password>@ahs786student.qh8rsrb.mongodb.net/?retryWrites=true&w=majority&appName=AHS786Student"
+uri = os.environ["MONGODB_URI"]  # må være satt fra før
+client = MongoClient(uri, tlsCAFile=certifi.where())
 
-client = MongoClient(MONGO_URI)
-db = client.ind320
-col = db.weather_curated
-
-doc = {"station_id":"oslo-01","ts":datetime.now(UTC),"temp_c":7.3,"source":"smoketest"}
-res = col.insert_one(doc)
-print("Inserted _id:", res.inserted_id)
-print("One doc:", col.find_one({}, {"_id":0}))
-print("Total docs:", col.count_documents({}))
+coll = client["ind320"]["ping"]
+coll.insert_one({"_id": 901, "note": "hello from python (TLS OK)"})
+print(coll.find_one({"_id": 901}))
+print("✅ MongoDB fungerer fra Python med riktig CA")
